a9ddb852f72a503d7b6cc219b386e6b05d2de2c8
loic lefloch <l.lefloch.91@gmail.com>
2019-04-26 13:53:05 +0200

    remove integration poc

--- integration/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: 'jest-puppeteer',
-  testRegex: './*\\.test\\.js$',
-  // https://github.com/smooth-code/jest-puppeteer/tree/master/packages/expect-puppeteer
-  setupTestFrameworkScriptFile: './setup.js',
-}

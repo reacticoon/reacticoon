c9489f4c3a5fed25ad07f91517a2426aab6c0e8b
Loic <l.lefloch.91@gmail.com>
2019-04-27 13:15:27 +0200

    update DevPluginApiMockDashboard: list files

--- src/reacticoon-mock-api-plugin/pages/DevPluginApiMockDashboard/DevPluginApiMockDashboard.js
+++ src/reacticoon-mock-api-plugin/pages/DevPluginApiMockDashboard/DevPluginApiMockDashboard.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
 import Page from 'reacticoon/reacticoon-dev-plugin/components/Page'
+import CommandContainer from 'reacticoon/reacticoon-dev-plugin/modules/command/view/CommandContainer'
 
 class DevPluginApiMockDashboard extends React.Component {
   render() {
-    return <Page title="Api mock dashboard">It works !</Page>
+    return (
+      <Page title="Api mock dashboard">
+        It works !
+        <CommandContainer command="MOCKAPI::LIST_FILES">
+          {({ report }) => <div>{JSON.stringify(report, null, 2)}</div>}
+        </CommandContainer>
+      </Page>
+    )
   }
 }
 

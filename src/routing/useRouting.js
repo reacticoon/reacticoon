f82f6ca0b9c4dfc5dc4fbeb828606121d62c76ab
Loic Lefloch <l.lefloch.91@gmail.com>
2022-04-02 01:55:13 +0200

    add useRouting

---
+++ src/routing/useRouting.js
@@ -0,0 +1,28 @@
+import { useDispatch } from 'react-redux'
+
+import {
+  redirectTo,
+  replaceWith,
+  reloadTo,
+  goBack,
+  // openOnNewTab,
+  openExternalLink,
+  redirectToExternal,
+  updatePageQueries,
+} from './actions.js'
+
+function useRouting() {
+	const dispatch = useDispatch()
+
+	return {
+		redirectTo: (...args) => dispatch(redirectTo.apply(null, args)),
+		replaceWith: (...args) => dispatch(replaceWith.apply(null, args)),
+		reloadTo: (...args) => dispatch(reloadTo.apply(null, args)),
+		goBack: (...args) => dispatch(goBack.apply(null, args)),
+		openExternalLink: (...args) => dispatch(openExternalLink.apply(null, args)),
+		redirectToExternal: (...args) => dispatch(redirectToExternal.apply(null, args)),
+		updatePageQueries: (...args) => dispatch(updatePageQueries.apply(null, args)),
+	}
+}
+
+export default useRouting
